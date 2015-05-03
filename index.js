e4cdebfc7223177068282523fe01266c249742fc
khanh-nguyen <khanh.nguyen@opower.com>
2015-05-03 15:00:41 -0400

    bot search image

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var Slack = require('slack-client'),
     bunyan = require('bunyan'),
     log = bunyan.createLogger({name: 'slackbot'}),
+    imageSearch = require('./lib/image-search'),
 
     // constants
-    constants = require('./constants'),
+    constants = require('./lib/constants'),
+    BOT_NAME = constants.BOT_NAME,
     CRON_MESSAGE = constants.CRON_MESSAGE,
     CRON_TIME = constants.CRON_TIME,
 
@@ -34,6 +36,22 @@ slack.on('open', function() {
 
 slack.on('error', function(error) {
     log.error({error: error}, 'slackbot experienced some error.');
+});
+
+slack.on('message', function(message) {
+    var channel = slack.getChannelGroupOrDMByID(message.channel),
+        msg = message.text,
+        IMG_MSG_REGEX = new RegExp('^' + BOT_NAME + '( image| img| animate)( me)? (.*)'),
+        imgMatch = msg.match(IMG_MSG_REGEX);
+
+    if (imgMatch && imgMatch[3]) {
+        imageSearch(imgMatch[3], function(err, img) {
+            if (!err) {
+                log.info({image: img}, 'csrbot send image');
+                channel.send(img);
+            }
+        });
+    }
 });
 
 slack.login();
