a76d16facdcae59eff9682182a4a80f6b712f3d9
khanh-nguyen <khanh.nguyen@opower.com>
2015-05-10 13:25:35 -0400

    add youtubeSearch

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Slack = require('slack-client'),
     bunyan = require('bunyan'),
     log = bunyan.createLogger({name: 'slackbot'}),
     imageSearch = require('./lib/image-search'),
+    youtubeSearch = require('./lib/youtube-search'),
     greeting = require('./lib/greeting'),
 
     // constants
@@ -22,7 +23,7 @@ slack.on('open', function() {
 });
 
 slack.on('error', function(error) {
-    log.error({error: error}, 'slackbot experienced some error.');
+    log.error({error: error}, BOT_NAME + ' experienced some error.');
 });
 
 slack.on('message', function(message) {
@@ -34,13 +35,24 @@ slack.on('message', function(message) {
 
     var msg = message.text,
         IMG_MSG_REGEX = new RegExp('^' + BOT_NAME + '( image| img| animate)( me)? (.*)'),
-        imgMatch = msg.match(IMG_MSG_REGEX);
+        YT_MSG_REGEX = new RegExp('^' + BOT_NAME + '( youtube| yt)( me)? (.*)'),
+        imgMatch = msg.match(IMG_MSG_REGEX),
+        ytMatch = msg.match(YT_MSG_REGEX);
 
     if (imgMatch && imgMatch[3]) {
-        imageSearch(imgMatch[3], function(err, img) {
+        return imageSearch(imgMatch[3], function(err, img) {
             if (!err) {
-                log.info({image: img}, 'csrbot send image');
+                log.info({image: img}, BOT_NAME + ' sends image');
                 channel.send(img);
+            }
+        });
+    }
+
+    if (ytMatch && ytMatch[3]) {
+        return youtubeSearch(ytMatch[3], function(err, video) {
+            if (!err) {
+                log.info({video: video}, BOT_NAME + ' sends video.');
+                channel.send(video);
             }
         });
     }
